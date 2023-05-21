d17ffb582e3f5e679068906a2686b2a83b82d68d
AndrewPrymenko <ffeelex@gmail.com>
2023-05-21 14:24:49 +0200

    #lessons 5-6

---
+++ Lesson5/student.py
@@ -0,0 +1,5 @@
+class Pupil:
+    pass
+class Student:#(Pupil):
+    pass
+
---
+++ Lesson6/main.py
@@ -0,0 +1,39 @@
+import warnings
+'''
+try:
+    your_code #requests, database, arythmetic proccess... 
+except ...:
+    proc_error/exception
+'''
+'''
+while(True):
+    try:
+        digit1 = 10
+        #warnings.warn("\nPossible zero division!\n")
+        digit2 = int(input("Enter digit: "))
+        res = digit1/digit2
+        print(res)
+    except ZeroDivisionError as zde:
+        print(f"Custom proccess: {zde}")
+    except Exception as ex:
+        print(ex)
+    yes = input("Enter Y for continue or N for stop: ")
+    if(yes.lower() == "n"):
+        break
+print("Cycle while has stoped!")
+'''
+from buildingerror import BuildingError
+from validation import Validation
+limit = 10
+amount = int(input("Enter amount: "))
+check = Validation(amount, limit)
+try:
+    check.Check()
+except ZeroDivisionError as zde:
+    print(f"ZeroDivisionError: {zde}")
+except BuildingError as be:
+    print(f"Custom error message\n"
+          f"{be}")
+except Exception as ex:
+    print(ex)
+
